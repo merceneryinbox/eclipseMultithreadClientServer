1ebc74cbb97e3db7d344315f4f0cd09dc7a2c530
mercenery <mercenery@inbox.ru>
2017-06-11 19:59:55 +0300

    delete unusable import

--- DicApServerTest/src/MultiThreadServer.java
+++ DicApServerTest/src/MultiThreadServer.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
